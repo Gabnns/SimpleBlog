a99ea424215b207a600a0928f45d6fb72bb65477
#Gab_nns <gabrielchaves204@gmail.com>
2023-08-21 08:45:41 -0300

    Criado função de pesquisa na NavBar

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 
 
+function search() {
+    let input = document.getElementById('NavBar').value;
+    
+
+    let x = document.getElementsByClassName('box');
+
+    for(i = 0; i < x.length; i++){
+        if(x[i].innerHTML.toLowerCase().includes(input.toLowerCase())){ // AQUI ELE PEGA O QUE FOR INSERIDO COMO TEXTO HTML DENTRO DA BARRA DE PESQUISA(INCLUDES) E BUSCA NO MAIN O CONTEUDO (BOX) COM AS LETRAS DIGITADAS, INDEPENDENTE SE MAIUSCULAS OU MINUSCULAS(toLowerCase);
+            x[i].style.display = 'block';
+        }else {
+            x[i].style.display = 'none';
+        }
+    }
+}
 
 function favoritar(){
     const icon = document.querySelectorAll(".fa-regular");
